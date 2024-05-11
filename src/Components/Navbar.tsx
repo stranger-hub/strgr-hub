d4778a7ccf41310715f0acac40e45532c7e15218
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2024-05-11 12:06:33 +0530

    init

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,8 @@
+"use client;"
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <button className="btn btn-primary">Hello daisyUI!</button>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <button className="btn btn-primary">Hello daisyUI!</button>
+      <div className="p-2 text-primary-red">knsdksjndksd</div>
+    </main>
+  );
+}
