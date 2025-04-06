13a5cb577c19e4363bd400765f8e73eb20924d65
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2025-04-06 17:23:33 +0530

    remove: public access to s3 keys

--- src/lib/s3Upload.ts
+++ src/lib/s3Upload.ts
@@ -1,15 +1,15 @@
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 export const uploadToS3 = async (file: File, userId: string): Promise<string | null> => {
   try {
     const params = {
-      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
+      Bucket: process.env.AWS_BUCKET_NAME!,
       Key: `pfp/${userId}/profie_pic`,
       Body: file,
       ContentType: file.type,
